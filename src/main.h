9b41f8816497e61e324f8d32c74055feb1384286
larryl79 <larryl79@gmail.com>
2023-12-02 23:01:01 +0000

    Delete src/main.h

--- src/main.h
+++
@@ -1,6 +0,0 @@
-#include <arduino.h>
-void setOutputPin(unsigned short int a, unsigned short int deflevel);
-void setChipEnable(unsigned short int enabled);
-void LatchPulse(unsigned short int);
-void setAddr(unsigned int addr);
-byte readEEPROM(int addr);
